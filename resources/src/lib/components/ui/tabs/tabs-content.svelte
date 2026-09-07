6c220ee36a803dacc088fb01a7e64b9dd727ddeb
LANREN <2118254764@qq.com>
2026-09-07 19:05:28 +0800

    为 Tabs 组件内容切换添加微动画

--- resources/src/lib/components/ui/tabs/tabs-content.svelte
+++ resources/src/lib/components/ui/tabs/tabs-content.svelte
@@ -12,6 +12,6 @@
 <TabsPrimitive.Content
 	bind:ref
 	data-slot="tabs-content"
-	class={cn("text-sm flex-1 outline-none", className)}
+	class={cn("text-sm flex-1 outline-none data-[state=active]:animate-in data-[state=inactive]:animate-out data-[state=active]:fade-in data-[state=inactive]:fade-out duration-200 ease-in-out", className)}
 	{...restProps}
 />
